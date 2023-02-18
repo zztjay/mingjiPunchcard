fba515d2e0091f8a94ad0ef665428ebca6b8263e
zztjay <zztjay@163.com>
2023-02-18 22:28:47 +0800

    aspect不生效问题解决

--- src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
+++ src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
@@ -2,9 +2,11 @@ package com.tencent.wxcloudrun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 @MapperScan(basePackages = {"com.tencent.wxcloudrun.dao"})
 public class WxCloudRunApplication {  
 

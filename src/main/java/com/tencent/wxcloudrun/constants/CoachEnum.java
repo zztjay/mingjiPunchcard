4f78a959335dbcd82027eb12d9cdcec637a987fc
zztjay <zztjay@163.com>
2023-02-19 00:27:09 +0800

    教练逻辑

--- src/main/java/com/tencent/wxcloudrun/constants/CoachEnum.java
+++ src/main/java/com/tencent/wxcloudrun/constants/CoachEnum.java
@@ -9,7 +9,9 @@ import lombok.Data;
 
 public enum CoachEnum {
     guohui("1","国晖"),
-    yinshao("2","英少");
+    yinshao("2","英少"),
+    taohe("oOPIl41B88kz0n-1mscSTsH3fJeM","周韬"),
+    shicong("oOPIl45BU7yfmV-0bYYKX-Os64G0","邱诗翀");
     private String openId;
     private String name;
 

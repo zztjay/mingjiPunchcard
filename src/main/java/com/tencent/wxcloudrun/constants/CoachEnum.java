36754c762ff150a2170a731c5305220000064267
zztjay <zztjay@163.com>
2023-02-20 14:06:06 +0800

    增加英少

--- src/main/java/com/tencent/wxcloudrun/constants/CoachEnum.java
+++ src/main/java/com/tencent/wxcloudrun/constants/CoachEnum.java
@@ -9,7 +9,7 @@ import lombok.Data;
 
 public enum CoachEnum {
     guohui("oOPIl435VLRHenkhYNHybgC2J_GU","国晖"),
-    yinshao("2","英少"),
+    yinshao("oOPIl4wkcibp4hbkb3ORbCYD6-B8","英少"),
     taohe("oOPIl41B88kz0n-1mscSTsH3fJeM","周韬"),
     shicong("oOPIl45BU7yfmV-0bYYKX-Os64G0","邱诗翀");
     private String openId;
--- src/main/java/com/tencent/wxcloudrun/constants/SuperManagerEnum.java
+++ src/main/java/com/tencent/wxcloudrun/constants/SuperManagerEnum.java
@@ -8,7 +8,7 @@ package com.tencent.wxcloudrun.constants;
 
 public enum SuperManagerEnum {
     guohui("oOPIl435VLRHenkhYNHybgC2J_GU","国晖"),
-    yinshao("2","英少"),
+    yinshao("oOPIl4wkcibp4hbkb3ORbCYD6-B8","英少"),
     taohe("oOPIl41B88kz0n-1mscSTsH3fJeM","周韬"),
     shicong("oOPIl45BU7yfmV-0bYYKX-Os64G0","邱诗翀");
     private String openId;
